3c26d7280edc63b2aaec618466164b4462005498
Eric N. Brown <eric.n.brown@gmail.com>
2015-09-09 08:40:39 -0500

    Removed some parameters from plot.images that are no longer relevant given how data scaling is performed when reading images. All data is now scaled without the initial raw data available. In addition, all data from the cornea is now removed at read time. Both of these changes to reading were made (previously) to account for the partial-volume effect of the cornea (similar to anti-aliasing).

--- R/plot.images.R
+++ R/plot.images.R
@@ -2,9 +2,7 @@
 #'
 #' @param x       CanaogramGAM to plot
 #' @param frames  Which frames to show
-#' @param scale   Which image scale to use (raw intensities or scalled intensities)
 #' @param masked  Show just the masked data or all data
-#' @param cornea  Include the cornea in the output (or omit it)
 #' @param ...     <not used>
 #'
 #' @return A ggplot2 object of the plot
@@ -20,14 +18,11 @@
 #' }
 plot.images <- function(x,
                         frames = c(1, round(max(x$gam$data$t) / 2), max(x$gam$data$t)),
-                        scale = c('scaled', 'raw'),
                         masked = FALSE,
-                        cornea = FALSE,
                         ...) {
   if (! inherits(x, 'CanaogramGAM')) {
     stop('Function plot.images requires a CanaogramGAM object\n')
   }
-  scale <- match.arg(scale)
 
   x <- x$gam$data
 
@@ -42,33 +37,18 @@ plot.images <- function(x,
           legend.margin = grid::unit(0, 'mm'),
           plot.margin = grid::unit(c(0,0,0,0), 'mm'))
 
-  if (scale == 'raw') {
-    title <- 'Raw Data'
-    x$z <- x$I
-  } else if (scale == 'scaled') {
-    title <- 'Scaled Data'
-  }
+  title <- 'Scaled Data'
 
   if (masked) {
-    x <- x[, x$mask == TRUE, drop = FALSE]
+    x <- x[x$mask == TRUE,]
   }
   x <- x[x$t %in% frames,]
-  if (cornea) {
-    x$fill <- ifelse(x$cornea, x$z + 2, x$z)
-    p <- ggplot(x, aes(x = x, y = y, fill = fill)) +
-      coord_fixed(ratio = 1) +
-      ggtitle(title) + xlab("") + ylab("") +
-      geom_raster() + facet_wrap(~ t) +
-      scale_fill_gradientn("Signal", colours=c('#000000', '#FFFFFF', '#000000', '#FF0000'), values = c(0, 1/3, 2/3, 3/3)) +
-      mytheme
-  } else {
-    p <- ggplot(x, aes(x = x, y = y, fill = z)) +
-      coord_fixed(ratio = 1) +
-      ggtitle(title) + xlab("") + ylab("") +
-      geom_raster() + facet_wrap(~ t) +
-      scale_fill_gradient("Signal", low = '#000000', high = '#FFFFFF') +
-      mytheme
-  }
+  p <- ggplot(x, aes(x = x, y = y, fill = z)) +
+    coord_fixed(ratio = 1) +
+    ggtitle(title) + xlab("") + ylab("") +
+    geom_raster() + facet_wrap(~ t) +
+    scale_fill_gradient("Signal", low = '#000000', high = '#FFFFFF') +
+    mytheme
   return(p)
 }
 
